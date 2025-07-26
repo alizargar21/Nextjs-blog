e7f5338f4bb35955d77e750f6884615f46f42406
Ali Zargar <93904858+alizargar21@users.noreply.github.com>
2025-07-26 16:15:03 +0330

    added fonts added color variable and absolute import config

---
+++ nextjs-blog-app/src/components/Header.jsx
@@ -0,0 +1,58 @@
+"use client";
+
+// import { useAuth } from "context/AuthContext";
+import NavLink from "./NavLink";
+
+const navLinks = [
+  {
+    id: 1,
+    children: "خانه",
+    path: "/",
+  },
+  {
+    id: 2,
+    children: "بلاگ ها",
+    path: "/blogs",
+  },
+  {
+    id: 3,
+    children: "درباره ما",
+    path: "/about",
+  },
+];
+
+function Header() {
+  // const { user, isLoading } = useAuth();
+
+  return (
+    <header
+      className={`z-10 shadow-md bg-inherit mb-10 sticky top-0
+         transition-all duration-200 border-b border-b-secondary-300 
+      `}
+    >
+      <nav className="container xl:max-w-screen-xl">
+        <ul className="flex items-center justify-between py-2 text-secondary-400">
+          <div className="flex items-center gap-x-10">
+            {navLinks.map((navLink) => {
+              return (
+                <li key={navLink.id}>
+                  <NavLink path={navLink.path}>{navLink.children}</NavLink>
+                </li>
+              );
+            })}
+          </div>
+          <li>
+            {/* {user ? (
+              <NavLink path="/profile">پروفایل</NavLink>
+            ) : (
+              <NavLink path="/signin">ورود</NavLink>
+            )} */}
+            <NavLink path="/signin">ورود</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
+// ${isLoading ? "blur-sm opacity-70" : "opacity-100 blur-0"}
---
+++ nextjs-blog-app/src/ui/Search.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+
+export default function Search() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const search = e.target.search; //* => search this part meaning input name
+    const searchValue = search.value;
+    const newParams = new URLSearchParams(searchParams.toString());
+    if (searchValue) {
+      newParams.set("search", searchValue);
+    } else {
+      newParams.delete("search");
+    }
+
+    router.push(pathname + "?" + newParams.toString(), { scroll: false });
+    // router.push(`${pathname}?${newParams.toString()}`, { scroll: false });
+  };
+
+  return (
+    <form className="relative" onSubmit={formSubmit}>
+      <input
+        type="text"
+        name="search"
+        placeholder="جستجو ..."
+        autoComplete="off"
+        className="py-3 text-xs textField__input bg-secondary-0"
+      />
+      <button
+        type="submit"
+        className="absolute top-0 left-0 flex items-center h-full ml-3"
+      >
+        <MagnifyingGlassIcon className="h-4 text-secondary-400" />
+      </button>
+    </form>
+  );
+}
